7d4facab41a48f0123df6d4f9c8fe53a32af75e1
JunYong Sun <ssjy4974@naver.com>
2023-08-07 15:12:07 +0900

    update : SingleKey, CompositeKey

--- src/main/java/com/bit/shop/domain/keys/SingleKey.java
+++ src/main/java/com/bit/shop/domain/keys/SingleKey.java
@@ -1,5 +1,10 @@
 package com.bit.shop.domain.keys;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class SingleKey<ID extends Number> implements EntityKey {
-    private ID id;
+    private final ID id;
 }
