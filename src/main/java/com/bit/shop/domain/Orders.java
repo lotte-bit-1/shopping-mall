cc3579f7fedeeb3c2ada9288aa5819c52686f717
MJun111 <kmj0568@gmail.com>
2023-08-07 14:21:17 +0900

    feat: add member and order

--- src/main/java/com/bit/shop/domain/Orders.java
+++ src/main/java/com/bit/shop/domain/Orders.java
@@ -1,7 +1,18 @@
 package com.bit.shop.domain;
 
 import com.bit.shop.domain.keys.SingleKey;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
 public class Orders extends BaseEntity<SingleKey<Long>> {
-
+    private Long id;
+    private Long userId;
+    private LocalDateTime regDate;
+    private String status;
 }
