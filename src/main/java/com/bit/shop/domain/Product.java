f9262bc60eaab0e6971184b9d7523aa77e4cd609
nowgnas <dev.nowgnas@gmail.com>
2023-08-07 14:14:45 +0900

    add product order attr

--- src/main/java/com/bit/shop/domain/Product.java
+++ src/main/java/com/bit/shop/domain/Product.java
@@ -1,9 +1,12 @@
 package com.bit.shop.domain;
 
 import com.bit.shop.domain.keys.SingleKey;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
-
+@Getter
+@RequiredArgsConstructor
 public class Product extends BaseEntity<SingleKey<Long>> {
     private Long categoryId;
     private String name;
--- src/main/java/com/bit/shop/domain/ProductOrder.java
+++ src/main/java/com/bit/shop/domain/ProductOrder.java
@@ -1,8 +1,14 @@
 package com.bit.shop.domain;
 
 import com.bit.shop.domain.keys.SingleKey;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public class ProductOrder extends BaseEntity<SingleKey<Long>> {
-
+    private long parentId;
+    private String name;
+    private int level;
 
 }
