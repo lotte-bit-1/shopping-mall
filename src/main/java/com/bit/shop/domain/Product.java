403c06c6a1ba181b46403d344f94f2e8c1fce8fa
nowgnas <dev.nowgnas@gmail.com>
2023-08-07 14:11:31 +0900

    add product attr

--- src/main/java/com/bit/shop/domain/Product.java
+++ src/main/java/com/bit/shop/domain/Product.java
@@ -2,6 +2,14 @@ package com.bit.shop.domain;
 
 import com.bit.shop.domain.keys.SingleKey;
 
+import java.time.LocalDateTime;
+
 public class Product extends BaseEntity<SingleKey<Long>> {
+    private Long categoryId;
+    private String name;
+    private long price;
+    private int quantity;
+    private LocalDateTime regDate;
+    private String code;
 
 }
--- src/main/java/com/bit/shop/domain/ProductOrder.java
+++ src/main/java/com/bit/shop/domain/ProductOrder.java
@@ -4,4 +4,5 @@ import com.bit.shop.domain.keys.SingleKey;
 
 public class ProductOrder extends BaseEntity<SingleKey<Long>> {
 
+
 }
