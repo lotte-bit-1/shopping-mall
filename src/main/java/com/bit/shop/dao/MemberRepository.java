4184ba36d4281bd1687617af6998a9583e3d7cfd
JunYong Sun <ssjy4974@naver.com>
2023-08-07 14:45:13 +0900

    update : import optimaze

--- src/main/java/com/bit/shop/dao/MemberRepository.java
+++ src/main/java/com/bit/shop/dao/MemberRepository.java
@@ -1,9 +1,6 @@
 package com.bit.shop.dao;
 
-import com.bit.shop.domain.BaseEntity;
-import com.bit.shop.domain.Cart;
 import com.bit.shop.domain.Member;
-import com.bit.shop.domain.keys.EntityKey;
 import com.bit.shop.domain.keys.SingleKey;
 import java.util.List;
 import java.util.Optional;
@@ -11,28 +8,28 @@ import java.util.Optional;
 public class MemberRepository implements DaoFrame<SingleKey<Long>, Member> {
 
 
-  @Override
-  public Optional<Member> getById(SingleKey<Long> key) {
-    return Optional.empty();
-  }
+    @Override
+    public Optional<Member> getById(SingleKey<Long> key) {
+        return Optional.empty();
+    }
 
-  @Override
-  public List<Member> getAll() {
-    return null;
-  }
+    @Override
+    public List<Member> getAll() {
+        return null;
+    }
 
-  @Override
-  public void insert(Member object) {
+    @Override
+    public void insert(Member object) {
 
-  }
+    }
 
-  @Override
-  public void delete(SingleKey<Long> key) {
+    @Override
+    public void delete(SingleKey<Long> key) {
 
-  }
+    }
 
-  @Override
-  public void update(Member object) {
+    @Override
+    public void update(Member object) {
 
-  }
+    }
 }
