5a062fdf4ae4269660537625a5f4fd8abb775530
CokeLee777 <akak640@kyonggi.ac.kr>
2023-08-07 14:09:33 +0900

    feat: add cart and category properties

--- src/main/java/com/bit/shop/domain/Cart.java
+++ src/main/java/com/bit/shop/domain/Cart.java
@@ -1,7 +1,14 @@
 package com.bit.shop.domain;
 
 import com.bit.shop.domain.keys.SingleKey;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public class Cart extends BaseEntity<SingleKey<Long>> {
 
+    private final int productQuantity;
+    private final Long productId;
+    private final Long memberId;
 }
--- src/main/java/com/bit/shop/domain/Category.java
+++ src/main/java/com/bit/shop/domain/Category.java
@@ -1,7 +1,14 @@
 package com.bit.shop.domain;
 
 import com.bit.shop.domain.keys.SingleKey;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public class Category extends BaseEntity<SingleKey<Long>> {
 
+    private final String name;
+    private final int level;
+    private Long parentId;
 }
