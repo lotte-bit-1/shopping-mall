7bddff7003cc8f731416bf988b0ac5cb98242acb
JunYong Sun <ssjy4974@naver.com>
2023-08-07 14:12:44 +0900

    Coupon, Likes 필드 추가

--- src/main/java/com/bit/shop/domain/Coupon.java
+++ src/main/java/com/bit/shop/domain/Coupon.java
@@ -1,7 +1,17 @@
 package com.bit.shop.domain;
 
 import com.bit.shop.domain.keys.SingleKey;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
+@Builder
 public class Coupon extends BaseEntity<SingleKey<Long>> {
-
+    private Long memberId;
+    private String name;
+    private String discountPolicy;
+    private int discountValue;
+    private boolean isUsed;
 }
--- src/main/java/com/bit/shop/domain/Likes.java
+++ src/main/java/com/bit/shop/domain/Likes.java
@@ -2,7 +2,14 @@ package com.bit.shop.domain;
 
 import com.bit.shop.domain.keys.CompositeKey;
 import com.bit.shop.domain.keys.SingleKey;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
+@Builder
 public class Likes extends BaseEntity<CompositeKey<Long>> {
-
+    private Long memberId;
+    private Long productId;
 }
